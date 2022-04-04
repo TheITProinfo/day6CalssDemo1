1fd38d7db37e474f6a6d913b46f618e6deb670ba
theitproinfo <theitproinfo@gmail.com>
2022-04-03 22:51:25 -0400

    Add project files.

---
+++ day6CalssDemo1/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6CalssDemo1
+{
+    class Person
+    {
+        public string _name;
+        public int _age;
+        public string _gender;
+        
+        public void Speak()
+        {
+
+            Console.WriteLine("I can talk");
+            Console.WriteLine("I am {0},age:{1}, gender:{2}",this._name,this._age,this._gender);
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("-------Driving---------");
+            Console.WriteLine("I can drive");
+        }
+
+
+
+
+    }
+}
---
+++ day6CalssDemo1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6CalssDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person tom = new Person();
+            tom._name = "Tom";
+            tom._age = 25;
+            tom._gender = "male";
+            tom.Speak();
+            tom.Drive();
+
+            Person jerry = new Person();
+            jerry._name = "Jerry";
+            jerry._age = -28;
+            jerry._gender = "Spring";
+            jerry.Speak();
+            jerry.Drive();
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+    
+}
